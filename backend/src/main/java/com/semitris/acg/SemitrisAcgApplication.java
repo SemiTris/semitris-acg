98bacc8dbe4d874e4a47871129ef2cdfb74047f7
bingzangzz <3150337376@QQ.COM>
2026-08-27 21:03:11 +0800

    feat: 初始化后端工程（Spring Boot + MyBatis + PageHelper）

---
+++ backend/src/main/java/com/semitris/acg/SemitrisAcgApplication.java
@@ -0,0 +1,13 @@
+package com.semitris.acg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemitrisAcgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SemitrisAcgApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/semitris/acg/SemitrisAcgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semitris.acg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemitrisAcgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
